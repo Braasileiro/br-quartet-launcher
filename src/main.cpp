f1df8b199f3c8d70a9aed9c6759f63d45480fa00
Lucas Cota <lucasbrunocota@live.com>
2026-08-16 23:49:44 -0300

    configure the subsystem as win32 executable add a manifest to resolve issues with COM initialization more reliably

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,45 @@
 #include <fstream>
 #include <map>
 #include <vector>
-#include <objbase.h>
 #include "logger.hpp"
 #include "resources.h"
 
-// General
-constexpr auto LOG_FILENAME{ "br-quartet-launcher.log" };
+// Constants
+constexpr auto LOG_FILENAME{"br-quartet-launcher.log"};
 
-// Window
-constexpr auto WINDOW_NAME{ "BLUE REFLECTION Quartet" };
-constexpr auto WINDOW_CLASS_NAME{ "DX11LauncherWndClass" };
+constexpr auto WINDOW_NAME{"BLUE REFLECTION Quartet"};
+constexpr auto WINDOW_CLASS_NAME{"DX11LauncherWndClass"};
 
-// Options
-constexpr auto OPT_QUARTET{ "quartet" };
-constexpr auto OPT_BR{ "br" };
-constexpr auto OPT_RAY{ "ray" };
-constexpr auto OPT_SUN{ "sun" };
-constexpr auto OPT_TIE{ "tie" };
+constexpr auto OPT_QUARTET{"quartet"};
+constexpr auto OPT_BR{"br"};
+constexpr auto OPT_RAY{"ray"};
+constexpr auto OPT_SUN{"sun"};
+constexpr auto OPT_TIE{"tie"};
 
-// DLLs
-constexpr auto DLL_QUARTET{ "B0.dll" };
-constexpr auto DLL_BR{ "B1.dll" };
-constexpr auto DLL_RAY{ "B2.dll" };
-constexpr auto DLL_SUN{ "B3.dll" };
-constexpr auto DLL_TIE{ "B4.dll" };
+constexpr auto DLL_QUARTET{"B0.dll"};
+constexpr auto DLL_BR{"B1.dll"};
+constexpr auto DLL_RAY{"B2.dll"};
+constexpr auto DLL_SUN{"B3.dll"};
+constexpr auto DLL_TIE{"B4.dll"};
 
 // Signatures
-typedef void (*FuncDllInitialize)(HWND*);
+typedef void (*FuncDllInitialize)(HWND *);
 typedef int (*FuncDllExecute)();
-typedef LRESULT(CALLBACK* FuncDllWndProc)(HWND, UINT, WPARAM, LPARAM);
+typedef LRESULT (CALLBACK*FuncDllWndProc)(HWND, UINT, WPARAM, LPARAM);
 
-// Global exception handler
-static LONG WINAPI CrashHandler(const EXCEPTION_POINTERS* ExceptionInfo) {
+// Utils
+static LONG WINAPI CrashHandler(const EXCEPTION_POINTERS *ExceptionInfo) {
     if (std::ofstream crashLog(LOG_FILENAME, std::ios::app); crashLog.is_open()) {
         crashLog << "fatal: Crashed! Exception code: 0x"
-            << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode
-            << std::dec << std::endl;
+                << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode
+                << std::dec << std::endl;
     }
 
     return EXCEPTION_EXECUTE_HANDLER;
 }
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int nShowCmd)
-{
+// Entry
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int nShowCmd) {
     // Setting the current working directory
     char exePathRaw[MAX_PATH];
     GetModuleFileNameA(nullptr, exePathRaw, MAX_PATH);
@@ -62,20 +58,13 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
     spdlog::info("br-quartet-launcher {} initialized", APP_VERSION_STR);
     spdlog::info("Command line: {}", GetCommandLineA());
 
-    // Initialize COM
-    if (HRESULT hrCom = CoInitializeEx(nullptr, COINIT_MULTITHREADED); hrCom < 0) {
-        spdlog::warn("CoInitializeEx() failed - HRESULT: 0x{:X}", static_cast<unsigned long>(hrCom));
-    } else {
-        spdlog::info("CoInitializeEx() success - HRESULT: 0x{:X}", static_cast<unsigned long>(hrCom));
-    }
-
     // Game mapping
     std::map<std::string, std::string> games = {
         {OPT_QUARTET, DLL_QUARTET},
-        {OPT_BR,      DLL_BR},
-        {OPT_RAY,     DLL_RAY},
-        {OPT_SUN,     DLL_SUN},
-        {OPT_TIE,     DLL_TIE}
+        {OPT_BR, DLL_BR},
+        {OPT_RAY, DLL_RAY},
+        {OPT_SUN, DLL_SUN},
+        {OPT_TIE, DLL_TIE}
     };
 
     std::string opt = OPT_QUARTET;
@@ -171,8 +160,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
 
             spdlog::info("The window fell into the void!");
         }
-    }
-    else {
+    } else {
         spdlog::error("Failed to create window. Error: {}", GetLastError());
     }
 
@@ -185,14 +173,18 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
 
     if (opt == OPT_QUARTET && exitCode > 1) {
         switch (exitCode) {
-        case 2: nextOpt = OPT_BR; break;
-        case 3: nextOpt = OPT_RAY; break;
-        case 4: nextOpt = OPT_SUN; break;
-        case 5: nextOpt = OPT_TIE; break;
-        default: nextOpt = ""; break;
+            case 2: nextOpt = OPT_BR;
+                break;
+            case 3: nextOpt = OPT_RAY;
+                break;
+            case 4: nextOpt = OPT_SUN;
+                break;
+            case 5: nextOpt = OPT_TIE;
+                break;
+            default: nextOpt = "";
+                break;
         }
-    }
-    else if (opt != OPT_QUARTET && exitCode == 1) {
+    } else if (opt != OPT_QUARTET && exitCode == 1) {
         // Return to the main launcher menu (B0.dll)
         nextOpt = OPT_QUARTET;
     }
@@ -204,7 +196,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
         std::vector cmdBuffer(cmdLineStr.begin(), cmdLineStr.end());
         cmdBuffer.push_back('\0');
 
-        STARTUPINFOA si = { .cb = sizeof(si) };
+        STARTUPINFOA si = {.cb = sizeof(si)};
         PROCESS_INFORMATION pi;
 
         auto process = CreateProcessA(
@@ -225,16 +217,12 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
             CloseHandle(pi.hThread);
 
             spdlog::info("Process spawned successfully");
-        }
-        else {
+        } else {
             spdlog::error("Failed to spawn process. Error: {}", GetLastError());
         }
     }
 
     spdlog::info("Exiting launcher...");
-
-    // Dispose things here
-    CoUninitialize();
 
     return 0;
 }
